08a00684d81b34bed5cff6a31e6a6c1b92cbbbb6
Laurel Morrison <lbentleym1992@gmail.com>
2021-04-02 11:22:45 -0400

    updating js

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Emma Benton</h3>
+        <div className="employee__shelter">Nashville Humane Society</div>
+    </section>
+)
---
+++ src/components/location/LocationCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Humane Society</h3>
+        <div className="location__address">Address: 123 Main Street</div>
+    </section>
+)
